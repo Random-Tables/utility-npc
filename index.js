756a95010f7b7a2d7b500edb1309c0f6d5860314
aron edwards <aronedwards91@gmail.com>
2022-08-13 14:32:48 +0100

    added to npm

---
+++ index.js
@@ -0,0 +1,17 @@
+const index = require("./index.json");
+const personality = require("./personality.json");
+const hobby = require("./hobby.json");
+const physical = require("./physical.json");
+const motivation = require("./motivation.json");
+const family = require("./family.json");
+
+exports.index = {
+  ...index,
+  tableData: {
+    personality,
+    hobby,
+    physical,
+    motivation,
+    family,
+  },
+};
